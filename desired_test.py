3f5d49f336443244a6c4d2bf4518384d4b98df6b
glenlcathey <glenlcathey@gmail.com>
2020-09-21 00:09:04 -0500

    set up test file for pushing desired states

---
+++ desired_test.py
@@ -0,0 +1,51 @@
+import subprocess
+import sys
+import json
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+install("paho-mqtt")
+
+import paho.mqtt.client as mqtt
+
+
+name = input("Enter the device name to push desired state to:")
+key = input("Enter key for desired state:")
+val = input("Enter value for desired state:")
+
+data = {
+        "state": {
+            "desired": {
+                key: val
+            }
+        }
+}
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("$devices/" + name + "/shadow/update/accepted")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+"   "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("localhost", 1883, 60)
+
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_start()
+while True:
+    input("press enter to send desired state again")
+    client.publish("$devices/" + name + "/shadow/update", json.dumps(data))
