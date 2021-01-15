f62619b432ba10a871c323d2fd101ff279001f41
glenlcathey <glenlcathey@gmail.com>
2021-01-14 18:29:48 -0600

    default_device file added

--- default_device.py
+++ default_device.py
@@ -2,29 +2,29 @@ from umqtt.simple import MQTTClient
 import ujson
 import utime
 
-data = {
+data = {                                                                                #NOTE this dict obj holds state data, change keys to reflect device state
     "state": {
         "reported": {
-            "counter": 0
+            "value": 0
         }
     }
 }
 
-ip = input("enter broker ip, if left blank defaults to home ip: ")
+ip = input("enter broker ip, if left blank defaults to saved broker ip: ")                      
 if (ip == ""):
-    ip = "192.168.1.2"
+    ip = ""     #NOTE put your broker ip here
 
 #port = input("enter the desired port, if left blank defualts to 1883: ")
 #if (port == ""):
     #port = 1883
-port = 1883
+port = 1883     #NOTE this is default mqtt port
 
-name = input("enter the device name for topics, if left blank defaults to esp1: ")
+name = input("enter the device name for topics, if left blank defaults to : ")          #NOTE change printed message to accurately reflect default val
 if (name == ""):
-    name = "esp1"
+    name = ""   #NOTE put preferred default device name here
 
 base_str = "devices/" + name + "/shadow/"    #this is the classic unnamed shadow
-shadow_list = []   #make the list to hold shadow names as they are received             #NOTE could be instantiated with values for shadows that should always exist (warning, critical, etc)
+shadow_list = []   #make the list to hold shadow names as they are received             #TODO could be instantiated with values for shadows that should always exist (warning, critical, etc)
 
 def on_message(topic, msg):
     topic = topic.decode("utf-8")
@@ -47,18 +47,17 @@ def parse_delta(x):
 def sub_to_named_shadow(shadow_name):
     c.subscribe(bytes(base_str + "name/" + shadow_name + "/update/delta"))
 
-c = MQTTClient(name, ip, port, b"counter_esp", b"counter_esp_password")
+c = MQTTClient(name, ip, port)                                                      #NOTE must insert username and password if using authenticated broker
 c.set_callback(on_message)
-c.set_last_will("devices/" + name + "/connected", b"0", qos=0, retain=True)        #can incorporate base string into this as well, just have to change connected on client side. 
+c.set_last_will("devices/" + name + "/connected", b"0", qos=0, retain=True)         #informs active clients the device has turned off in event of shutdown
 c.connect()
-c.publish(b'devices/' + name + '/connected', b'1', retain=True)                    #can incorporate base string into this as well, just have to change connected on client side. 
+c.publish(b'devices/' + name + '/connected', b'1', retain=True)                     #informs any active clients that device is now active
 c.subscribe(bytes(base_str + "update/delta", 'utf-8'))                              #subscribe to classic shadow update delta
 
-while True:
+while True:                                                                         #NOTE unique device behavior must be defined in this loop in order to continually execute
     state = ujson.dumps(data)
     c.publish(bytes(base_str + "update", 'utf-8'), state, qos=1)                                      #publish curr state to unnamed shadow
     for element in shadow_list:
         c.publish(bytes(base_str + "/name/" + element + "/update", 'utf-8'), state, qos=1)            #publish the current state to every known named shadow
-    data["state"]["reported"]["counter"] = data["state"]["reported"]["counter"] + 1
     utime.sleep(1)
     c.check_msg()
